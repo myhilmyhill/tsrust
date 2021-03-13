9347e4be4a23c571807d65575d102073e5ec18f6
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2021-03-13 17:31:12 +0000

    work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 
-fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+
+struct TsPacket {
+    reader: std::io::BufReader<std::fs::File>,
+}
+
+impl Iterator for TsPacket {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Vec<u8>> {
+        let mut buf: Vec<u8> = Vec::new();
+        let i = self.reader.read_until(b'G', &mut buf);
+        match i {
+            Ok(_) => Some(buf),
+            Err(_) => None,
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if let Some(path) = args.get(1) {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        
+        fn a(reader: std::io::BufReader<std::fs::File>) -> TsPacket {
+            TsPacket { reader }
+        }
+        for i in a(reader) {
+            println!("{:?}", i);
+        }
+    }
+    Ok(())
 }
