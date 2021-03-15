6cdaeba6da9d89549275948bc156f1a9c032c27f
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2021-03-15 15:21:39 +0000

    add TsHeader

--- src/ts/ts_packet.rs
+++ src/ts/ts_packet.rs
@@ -1,10 +1,14 @@
-pub struct TsPacket {
-    pub bytes: Vec<u8>,
-    // pub header: Vec<u8>,
-    // pub payload: Vec<u8>,
-}
+#[cfg(test)]
+mod tests;
 
 use std::fmt::*;
+
+pub struct TsPacket {
+    pub bytes: Vec<u8>,
+    pub header: TsHeader,
+    pub adaptation: TsAdaptationField,
+    pub payload: TsPayload,
+}
 
 impl Debug for TsPacket {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
@@ -13,4 +17,40 @@ impl Debug for TsPacket {
         }
         write!(f, "\n")
     }
+}
+
+#[derive(Debug)]
+pub struct TsHeader {
+    pub tei: bool,
+    pub pusi: bool,
+    pub tp: bool,
+    pub pid: u16, // 13 bit
+    pub tsc: u8,  // 2 bit
+    pub afc: u8,  // 2 bit
+    pub cc: u8,   // 4 bit
+    pub bytes: Vec<u8>,
+}
+
+impl From<Vec<u8>> for TsHeader {
+    fn from(bytes: Vec<u8>) -> Self {
+        let header = vec![bytes[0], bytes[1], bytes[2]];
+        TsHeader {
+            bytes,
+            tei: header[0] >> 7 == 1,
+            pusi: header[0] >> 6 & 1 == 1,
+            tp: header[0] >> 5 & 1 == 1,
+            pid: (((header[0] & 0x1f) as u16) << 8) as u16 | (header[1]) as u16,
+            tsc: header[2] >> 6,
+            afc: (header[2] >> 4) & 3,
+            cc: header[2] & 0xf,
+        }
+    }
+}
+
+pub struct TsAdaptationField {
+    pub bytes: Vec<u8>,
+}
+
+pub struct TsPayload {
+    pub bytes: Vec<u8>,
 }
---
+++ src/ts/ts_packet/tests.rs
@@ -0,0 +1,142 @@
+use super::*;
+
+#[test]
+fn reads_tei_as_true() {
+    let bytes = vec![0x80u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(true, header.tei);
+}
+
+#[test]
+fn reads_tei_as_false() {
+    let bytes = vec![0x00u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(false, header.tei);
+}
+
+#[test]
+fn reads_pusi_as_true() {
+    let bytes = vec![0x40u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(true, header.pusi);
+}
+
+#[test]
+fn reads_pusi_as_false() {
+    let bytes = vec![0x00u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(false, header.pusi);
+}
+
+#[test]
+fn reads_tp_as_true() {
+    let bytes = vec![0x20u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(true, header.tp);
+}
+
+#[test]
+fn reads_tp_as_false() {
+    let bytes = vec![0x00u8, 0x00, 0x00];
+    let header = TsHeader::from(bytes);
+    assert_eq!(false, header.tp);
+}
+
+#[test]
+fn reads_pid() {
+    struct Param {
+        bytes: Vec<u8>,
+        expected: u16,
+    }
+    for p in vec![
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x00],
+            expected: 0,
+        },
+        Param {
+            bytes: vec![0x1fu8, 0xff, 0x00],
+            expected: 0x1fff,
+        },
+    ] {
+        let header = TsHeader::from(p.bytes);
+        assert_eq!(p.expected, header.pid);
+    }
+}
+
+#[test]
+fn reads_tsc() {
+    struct Param {
+        bytes: Vec<u8>,
+        expected: u8,
+    }
+    for p in vec![
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x00],
+            expected: 0,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0xc0],
+            expected: 0b11,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x80],
+            expected: 0b10,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x40],
+            expected: 0b01,
+        },
+    ] {
+        let header = TsHeader::from(p.bytes);
+        assert_eq!(p.expected, header.tsc);
+    }
+}
+
+#[test]
+fn reads_afc() {
+    struct Param {
+        bytes: Vec<u8>,
+        expected: u8,
+    }
+    for p in vec![
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x00],
+            expected: 0,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x30],
+            expected: 0b11,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x20],
+            expected: 0b10,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x10],
+            expected: 0b01,
+        },
+    ] {
+        let header = TsHeader::from(p.bytes);
+        assert_eq!(p.expected, header.afc);
+    }
+}
+
+#[test]
+fn reads_cc() {
+    struct Param {
+        bytes: Vec<u8>,
+        expected: u8,
+    }    for p in vec![
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x00],
+            expected: 0,
+        },
+        Param {
+            bytes: vec![0x00u8, 0x00, 0x0f],
+            expected: 0xf,
+        },
+    ] {
+        let header = TsHeader::from(p.bytes);
+        assert_eq!(p.expected, header.cc);
+    }
+}
